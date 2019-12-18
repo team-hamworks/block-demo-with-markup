dd24a7cf0c392152a0f5a3c0a9b19e4e8deb743e
Hiroshi Urabe <urabe@ham.works>
2019-12-18 16:45:11 +0900

    Release 0.0.4

--- block-demo-with-markup.php
+++ block-demo-with-markup.php
@@ -7,7 +7,7 @@
  * Author URI:      https://ham.works
  * Text Domain:     block-demo-with-markup
  * Domain Path:     /languages
- * Version: 0.0.3
+ * Version: 0.0.4
  * @package         Block_Demo_With_Markup
  */
 
