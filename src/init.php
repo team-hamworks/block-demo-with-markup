3aa1cb91570f4533a7bf4a647070478a0ac5b107
Hiroshi Urabe <urabe@ham.works>
2020-01-06 15:48:34 +0900

    front js

--- src/init.php
+++ src/init.php
@@ -13,14 +13,30 @@ add_action(
 			$script_asset['dependencies'],
 			$script_asset['version']
 		);
+
 		// block の登録。
 		register_block_type( 'block-demo-with-markup/block-demo-with-markup', array(
 			'editor_script'   => 'block-demo-with-markup',
+			'script'          => 'block-demo-with-markup-front',
 			'style'           => 'block-demo-with-markup',
 			'render_callback' => __NAMESPACE__ . '\render'
 		) );
 		load_plugin_textdomain( 'block-demo-with-markup', false, basename( PLUGIN_FILE ) . '/languages' );
 		wp_set_script_translations( 'block-demo-with-markup', 'block-demo-with-markup', basename( PLUGIN_FILE ) . '/languages' );
+	}
+);
+
+add_action(
+	'init',
+	function () {
+		$script_asset = require( dirname( PLUGIN_FILE ) . '/build/front.asset.php' );
+		wp_enqueue_code_editor( array( 'type' => 'htmlmixed' ) );
+		wp_enqueue_script(
+			'block-demo-with-markup-front',
+			plugins_url( 'build/front.js', PLUGIN_FILE ),
+			array_merge( $script_asset['dependencies'], [ 'code-editor' ] ),
+			$script_asset['version']
+		);
 	}
 );
 
